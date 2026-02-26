ba424b5e85bd21d457f43cb3d344674d29f5b47c
Satvik Dubey <satvikdubey268@gmail.com>
2026-02-26 20:13:55 +0530

    fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,52 +4,54 @@ lock "~> 3.17"
 set :application, "spree"
 set :repo_url, "git@github.com:Dubeysatvik123/spree.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :deploy_to, "/home/deploy/spree"
 set :branch, "main"
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
+# Linked files and dirs
 set :linked_files, %w[config/database.yml config/master.key]
-
-# Default value for linked_dirs is []
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets public/system vendor/bundle public/uploads]
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
+
+# Bundler
 set :bundle_path, -> { shared_path.join("vendor/bundle") }
 
+# Puma
 set :puma_threads, [4, 16]
 set :puma_workers, 2
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log, "#{release_path}/log/puma.error.log"
+
+# Fixed: logs should live in shared_path so they persist across deploys
+set :puma_access_log, "#{shared_path}/log/puma.access.log"
+set :puma_error_log,  "#{shared_path}/log/puma.error.log"
+
 set :puma_systemctl_user, :system
 set :puma_enable_socket_service, true
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+
+# Rbenv
 set :rbenv_type, :user
 set :rbenv_ruby, '3.2.2'
 set :rbenv_path, '/home/deploy/.rbenv'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_roles, :all
+
+# Fix log permissions after shared symlinks are created
+namespace :deploy do
+  after 'deploy:symlink:shared', :fix_log_permissions do
+    on roles(:app) do
+      execute :chown, '-R', 'deploy:deploy', "#{shared_path}/log"
+      execute :chmod, '-R', '775', "#{shared_path}/log"
+    end
+  end
+
+  # Verify master.key exists on server before deploy proceeds
+  before :starting, :check_master_key do
+    on roles(:app) do
+      unless test("[ -f #{shared_path}/config/master.key ]")
+        raise "master.key is missing from #{shared_path}/config/! Upload it first."
+      end
+    end
+  end
+end
